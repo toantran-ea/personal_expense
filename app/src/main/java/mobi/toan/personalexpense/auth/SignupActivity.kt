2e40d21a233738a2823cdbaf3814370af8630ef4
Toan <me@toan.mobi>
2019-06-16 09:07:39 +0700

    WIP login firebase

---
+++ app/src/main/java/mobi/toan/personalexpense/auth/SignupActivity.kt
@@ -0,0 +1,72 @@
+package mobi.toan.personalexpense.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import mobi.toan.personalexpense.R
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+    }
+
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
+        }
+
+        showProgressDialog()
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user?)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // [START_EXCLUDE]
+                hideProgressDialog()
+                // [END_EXCLUDE]
+            }
+        // [END create_user_with_email]
+    }
+
+
+    private fun showProgressDialog() {
+
+    }
+
+    companion object {
+        const val TAG = "Signup"
+    }
+
+}
